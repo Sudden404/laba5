5555cac16e48fd0f3f3a709338071812c685f0e8
User <vlad00001234567890@mail.ru>
2021-12-06 19:36:30 +0300

    something new

--- main.c
+++ main.c
@@ -2,28 +2,44 @@
 #include <stdlib.h>
 #define N 10
 char* make_array(){
-    int str_len = 0,k,j = 0;
+    int str_len = 0,k,j = 0,i = 0,word_len = N,word_len_old;
     char c;
     char** array = NULL;
-    char* str = NULL;
-    array = malloc(N * sizeof(*array));
-    for (int i = 0; i < N; ++i) {
-        array[i] = malloc(N * sizeof(**array));
+    array = malloc(word_len * sizeof(*array));
+    for (int z = 0; z < N; ++z) {
+        array[z] = malloc(str_len * sizeof(**array));
     }
-    str = malloc(str_len*sizeof(char));
 
     start:
     str_len += N;
     k = N;
-    str = realloc(str,str_len*sizeof(char));
-    while ((k != 0) && ((c = getchar()) != 10)){
-        str[j++] = c;
+    array[i] = realloc(array[i],str_len*sizeof(char));
+    while ((k != 0) && ((c = getchar()) != 10) && (c != ' ')){
+        array[i][j++] = c;
         k--;
+    }
+    if (c == ' ') {
+        array[i][j] = 0;
+        j = 0;
+        str_len = 0;
+        if (i+1 < word_len)
+            i++;
+        else
+        {
+            word_len_old = word_len;
+            word_len += 3;
+            array = realloc(array,word_len * sizeof(*array));
+            for(int d = word_len_old;d<word_len;++d)
+                array[d] = malloc(sizeof(**array));
+            i++;
+        }
+        goto start;
+
     }
     if (k == 0)
         goto start;
-    else str[j] = 0;
-    return str;
+    else array[i][j] = 0;
+    return array[3];
 }
 
 int main() {
